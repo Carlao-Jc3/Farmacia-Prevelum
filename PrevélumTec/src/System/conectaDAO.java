57ac1e64651a91a7d89e8d5ae84d552d46a32a71
Carlos Eduardo <eduardocarlos@gmail.com>
2025-06-04 20:45:53 -0300

    conexão com DB, criei table pessoa

---
+++ PrevélumTec/src/System/conectaDAO.java
@@ -0,0 +1,27 @@
+package System;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class conectaDAO {
+    //Classe de conexão com o banco de dados uc11
+    public static Connection connectDB() {
+        try {
+            //Instância de conexão.
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/prevelum", "root" ,"CAmysql2025DU");
+            //A partir do DriverManager, ele recebe a senha e user e estabelece uma conexão estável com banco.
+            System.out.println("CONECTADO!");
+            return conn;
+        } catch (SQLException erro) {
+            //Captura de erro, gerando uma imagem de erro mais amigável e compreensível.
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados: " + erro.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            throw new RuntimeException("Falha na conexão com o banco de dados", erro);
+        }
+    }
+    
+}
