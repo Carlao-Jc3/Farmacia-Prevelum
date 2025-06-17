828b1c5a606802736cb6762ee63d6404d443b675
Carslo Eduardo <eduardocarlos.hg@gmail.com>
2025-06-17 14:40:12 -0300

    Criação classe Venda

--- PrevelumSW/src/main/java/System/Usuario.java
+++ PrevelumSW/src/main/java/System/Usuario.java
@@ -1,20 +1,32 @@
 package System;
 
 public class Usuario {
-    private String nome;
+    private int id;
+    private String usuario;
     private String senha;
+    private String nivelAcesso;
 
-    public Usuario(String nome, String senha) {
-        this.nome = nome;
+    public Usuario(int id, String usuario, String senha, String nivelAcesso) {
+        this.id = id;
+        this.usuario = usuario;
         this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
     }
 
-    public String getNome() {
-        return nome;
+    public int getId() {
+        return id;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
     public String getSenha() {
@@ -23,6 +35,14 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
     }
     
     
